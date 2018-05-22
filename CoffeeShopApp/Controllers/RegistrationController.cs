56ef74f036d94d98757abc5ef210fad78756af76
Anamul Islam <anamul.islam127@gmail.com>
2018-05-22 12:32:49 -0400

    First commit

---
+++ CoffeeShopApp/Controllers/RegistrationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeShopApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register(string firstName, string lastName, string email, int phoneNumber, string password)
+        {
+            ViewBag.FirstName = firstName;
+            ViewBag.LastName = lastName;
+            ViewBag.Email = email;
+            ViewBag.PhoneNumber = phoneNumber;
+            ViewBag.Password = password;
+            return View();
+        }
+
+    }
+}
